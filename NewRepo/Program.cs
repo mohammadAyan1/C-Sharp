a59437923d38c77fe4cf7c46dbe4a3d532dc1baa
mohammadAyan1 <mohammadayan2210@gmail.com>
2025-04-14 00:50:48 +0530

    Refactor Program.cs with comments and code adjustments
    Significantly modified `Program.cs` by adding comments to explain various C# concepts, including type casting and operators. Commented out existing code to disable functionality while retaining educational context. Added a new `using` directive and included active code demonstrating string manipulation.

--- NewRepo/Program.cs
+++ NewRepo/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -52,22 +53,95 @@ namespace NewRepo
             //there are two types of typeCasting
             //1, implicite casting
             //char to int to long to float to double
-            int x = 3;
-            double y = x;
-            int z = 'y';
-            Console.WriteLine(x);
-            Console.WriteLine(z);
+
+            //int x = 3;
+            //double y = x;
+            //int z = 'y';
+            //Console.WriteLine(x);
+            //Console.WriteLine(z);
 
 
             //2. explicit casting
-            int a = (int) 4.5;
-            Console.WriteLine(a);
-            float b = 'y';
-            float var = Convert.ToInt32(4.55);
-            Console.WriteLine(var);
+            //int a = (int) 4.5;
+            //Console.WriteLine(a);
+            //float b = 'y';
+            //float var = Convert.ToInt32(4.55);
+            //string sx = "Random String";
+            //Console.WriteLine(var);
+            //Console.WriteLine(b);
+            //Console.WriteLine(sx);
+
+            //Console.WriteLine("Enter your Name");
+            //string name =  Console.ReadLine();
+            //Console.WriteLine("Hey Hello " + name); 
+
+            //Console.WriteLine("How many candies do you want");
+            //int can = Convert.ToInt16(Console.ReadLine());
+            //Console.WriteLine("You will get 4 more candies " + (can+4));
+
+            // Operators in c#
+            // 1. Arithemetic Operator
+            // 2. assignment operator
+            // 3.Logical operator
+            // 4. comarison operator
 
 
+            //Arithmetic operator
+            //int a = 4;
+            //int b = 2;
+            //Console.WriteLine("The value of a + b is : " + (a + b));
+            //Console.WriteLine("The value of a + b is : " + (a - b));
+            //Console.WriteLine("The value of a + b is : " + (a / b));
+            //Console.WriteLine("The value of a + b is : " + (a * b));
 
+            //Assignment operator
+            //int a = 4;
+            //int b = a;
+            //b -= 4;
+            //b += 4;
+            //b *= 4;
+            //b /= 4;
+            //Console.WriteLine(b);
+
+            //Logical Operator
+            //Console.WriteLine(true && false);
+            //Console.WriteLine(true && true);
+            //Console.WriteLine(false && false);
+
+            //Console.WriteLine(true || false);
+            //Console.WriteLine(true || true);
+            //Console.WriteLine(false || false);
+
+
+            //Console.WriteLine(!false);
+            //Console.WriteLine(!true);
+
+            //Comparison operator
+            //Console.WriteLine(324>555);
+            //Console.WriteLine(324>=555);
+            //Console.WriteLine(324<=555);
+            //Console.WriteLine(324!=555);
+            //Console.WriteLine(324==555);
+
+            //Math
+            //int a = Convert.ToInt16( Math.Sqrt(4));//min,sqrt,abs
+            //Console.WriteLine(a);
+
+            //String method
+            //string hello = "Hello world this is Ayan";
+            //Console.WriteLine(hello.Length);
+            //Console.WriteLine(hello.ToUpper());
+            //Console.WriteLine(hello.ToLower());
+            //Console.WriteLine(string.Concat(hello," You are nice"));
+
+            //string name = Console.ReadLine();
+            //string candies = Console.ReadLine();
+            //console.writeline($"your name is {name}. you will get {candies} candies");
+
+            string hello = "Hello world this is Ayan";
+            Console.WriteLine(hello[1]);
+            Console.WriteLine(hello.IndexOf("is"));
+            Console.WriteLine(hello.Substring(1));
 
 
             Console.ReadLine();
